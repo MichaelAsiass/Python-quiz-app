0533a8d36ece424cb7ada376e617e930f48fb3f1
MichaelAsiass <46844692+MichaelAsiass@users.noreply.github.com>
2023-07-03 07:55:27 -0400

    Add files via upload

---
+++ quiz-test/index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ['Master Python with Fun Quiz Challenges', 'Elevate Your Python Knowledge','Improve Your Python Skills'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop: true
+  };
+var typed = new Typed('.typing', options);
---
+++ quiz-test/js/questions.js
@@ -0,0 +1,82 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Given a list numbers = [1, 2, 3, 4, 5], what will sum(numbers) return?",
+    answer: "15",
+    options: ["15", "5", "[1, 2, 3, 4, 5]", "TypeError"],
+  },
+  {
+    numb: 2,
+    question: "How do you remove an element from a set in Python?",
+    answer: "All of the above",
+    options: [
+      "Using the remove() method",
+      "Using the discard() method",
+      "Using the pop() method",
+      "All of the above",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      "Which of the following is the correct way to import the math module in Python?",
+    answer: "import math",
+    options: [
+      "import math",
+      "from math import *",
+      "from math import math",
+      "from math import sqrt, sin",
+    ],
+  },
+  {
+    numb: 4,
+    question: "Which of the following is used to read a CSV file in Python?",
+    answer: "csv.reader()",
+    options: ["csv.load()", "csv.open()", "csv.reader()", "csv.read()"],
+  },
+  {
+    numb: 5,
+    question: "What does the __init__ method do in a Python class?",
+    answer: "It initializes the object's attributes",
+    options: [
+      "It initializes the object's attributes",
+      "It defines the class methods",
+      "It is a destructor method",
+      "It is a static method",
+    ],
+  },
+  {
+    numb: 6,
+    question: "Which module in Python can be used for sending emails?",
+    answer: "smtplib",
+    options: [
+      "email",
+      "smtp",
+      "smtplib",
+      "poplib",
+    ],
+  },
+  {
+    numb: 7,
+    question: "What is the purpose of the pass statement in Python?",
+    answer: "It is a placeholder statement that does nothing.",
+    options: [
+      "It is used to skip the current iteration in a loop.",
+      "It is used to raise an exception.",
+      "It is used to terminate the program.",
+      "It is a placeholder statement that does nothing.",
+    ],
+  },
+  {
+    numb: 8,
+    question: "What is the output of the following code snippet numbers = [1, 2, 3, 4, 5] squared = [x ** 2 for x in numbers if x % 2 == 0]print(squared)",
+    answer: "[4, 16]",
+    options: [
+      "[1, 4, 9, 16, 25]",
+      "[4, 16]",
+      "[2, 4]",
+      "[16]",
+    ],
+  },
+];
